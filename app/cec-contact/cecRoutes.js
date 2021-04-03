574459510ee29c786fcdb7a494a35b12fed20739
Claudio Alexssandro Lino <codigosecafe@gmail.com>
2021-04-03 03:53:02 -0300

    [MASTER][UPD]

--- app/cec-contact/cecRoutes.js
+++ app/cec-contact/cecRoutes.js
@@ -5,10 +5,6 @@ angular.module('cecContactApp')
 	            templateUrl: 'app/cec-contact/modules/index/views/index.html',
 	            controller: 'cecIndexController as vmIndex'
 	        })
-	        .when('/index-en.html', {
-	            templateUrl: 'app/cec-contact/modules/index/views/index-en.html',
-	            controller: 'cecIndexController as vmIndex'
-	        })
 	        .otherwise({
 	            redirectTo: '/index.html'
 	        })
